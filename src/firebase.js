46ede5d54ae7abb268ba5250ee6f588ff0a29ef0
nishantrao2003 <nishantrao2003@gmail.com>
2025-10-12 13:27:43 +0000

    Init Commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA5WkdqDxJc3ZkRWjK_qQrV2qOxm3CF1wE",
-  authDomain: "autom8-app.firebaseapp.com",
-  projectId: "autom8-app",
-  storageBucket: "autom8-app.firebasestorage.app",
-  messagingSenderId: "185124421343",
-  appId: "1:185124421343:web:6e518367a4405050fa336a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
