01a095c9416e99b810bf761f34092509f8dec466
Nishant Rao <nishantrao2003@gmail.com>
2025-11-06 20:21:41 +0530

    Auth with basic Chatpage implemented 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
->>>>>>> 68e5ed4 (Auth with basic Chatpage implemented)
 import LandingPage from './views/landing_page/landing_page.jsx';
 import Login from './views/auth/login/login.jsx';
 import Signup from './views/auth/signup/signup.jsx';
 import './App.css';
-<<<<<<< HEAD
-import ChatPage from "./views/chat_page.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
-
-//Anything other than /, /login and /signup should be redirected to / if not authenticated
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route 
-          path="/chat" 
-          element={
-            <PrivateRoute>
-              <ChatPage />
-            </PrivateRoute>
-=======
 import ChatPage from "./views/chat_page/chat_page.jsx";
 import { useAuth } from "./context/AuthContext";
 import { useEffect } from "react";
@@ -119,7 +95,6 @@ function App() {
           path="/signup"
           element={
             (!isAuthenticated) ? <Signup /> : <Navigate to="/chat"  />
->>>>>>> 68e5ed4 (Auth with basic Chatpage implemented)
           }
         />
       </Routes>
